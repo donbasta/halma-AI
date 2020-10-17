fed8ca8c32f0485b4fdf4b360c2e451a8e3581ca
Muhammad Hasan <muhammadhasan50@gmail.com>
2020-10-17 15:29:58 +0700

    change state to board

--- js/Util.js
+++ js/Util.js
@@ -3,52 +3,52 @@ const INF = 99999999999;
 const DX = [0, 0, 1, -1, 1, 1, -1, -1];
 const DY = [1, -1, 0, 0, -1, 1, 1, -1];
 
-function findAvailablePositions(x, y, state) {
-    const BSize = state.length;
+function findAvailablePositions(x, y, board) {
+    const BSize = board.length;
     let retPositions = [];
-
-    if (state[x][y] == 0) return retPositions;
+  
+    if (board[x][y] == 0) return retPositions;
     
     let queue = [];
     queue.push([x, y, true]);
-
+  
     let visited = new Array(BSize);
     for (let i = 0; i < BSize; i++) {
-        visited[i] = new Array(BSize);
-        for (let j = 0; j < BSize; j++) {
-            visited[i][j] = 0;
-        }
+      visited[i] = new Array(BSize);
+      for (let j = 0; j < BSize; j++) {
+        visited[i][j] = 0;
+      }
     }
     visited[x][y] = 1;
-
+  
     while (queue.length > 0) {
-        const top = queue[0];
-        queue.shift();
-        const cx = top[0];
-        const cy = top[1];
-        const expand = top[2];
-        for (let i = 0; i < DX.length; i++) {
-            let tx = cx + DX[i];
-            let ty = cy + DY[i];
-            if (tx < 0 || ty < 0 || tx >= BSize || ty >= BSize) continue;
-            if (visited[tx][ty]) continue;
-            if (state[tx][ty] != 0) {
-                tx += DX[i], ty += DY[i];
-                if (tx < 0 || ty < 0 || tx >= BSize || ty >= BSize) continue;
-                if (visited[tx][ty]) continue;
-                if (state[tx][ty] != 0) continue;
-                visited[tx][ty] = 1;
-                retPositions.push([tx, ty]);
-                queue.push([tx, ty, false]);
-            } else if (expand) {
-                visited[tx][ty] = 1;
-                retPositions.push([tx, ty]);
-            }
+      const top = queue[0];
+      queue.shift();
+      const cx = top[0];
+      const cy = top[1];
+      const expand = top[2];
+      for (let i = 0; i < DX.length; i++) {
+        let tx = cx + DX[i];
+        let ty = cy + DY[i];
+        if (tx < 0 || ty < 0 || tx >= BSize || ty >= BSize) continue;
+        if (visited[tx][ty]) continue;
+        if (board[tx][ty] != 0) {
+          tx += DX[i], ty += DY[i];
+          if (tx < 0 || ty < 0 || tx >= BSize || ty >= BSize) continue;
+          if (visited[tx][ty]) continue;
+          if (board[tx][ty] != 0) continue;
+          visited[tx][ty] = 1;
+          retPositions.push([tx, ty]);
+          queue.push([tx, ty, false]);
+        } else if (expand) {
+          visited[tx][ty] = 1;
+          retPositions.push([tx, ty]);
         }
+      }
     }
-
+  
     return retPositions;
-}
+  }
 
 function positionInCorner(x, y, BSize, playerType) {
   if (playerType == 2) {
@@ -59,8 +59,8 @@ function positionInCorner(x, y, BSize, playerType) {
   return (x + y <= 5);
 }
 
-function utilityFunction(state) {
-  const BSize = state.length;
+function utilityFunction(board) {
+  const BSize = board.length;
   
   function F(playerType) {
     const otherPlayer = (3 - playerType);
@@ -71,7 +71,7 @@ function utilityFunction(state) {
 
     for (let i = 0; i < BSize; i++) {
       for (let j = 0; j < BSize; j++) {
-        if (state[i][j] == playerType) {
+        if (board[i][j] == playerType) {
           pawnPositions.push([i, j]);
         }
         if (positionInCorner(i, j, BSize, otherPlayer)) {
@@ -97,13 +97,13 @@ function utilityFunction(state) {
   return F(1) - F(2);
 }
 
-// let state = [
+// For Debugging Purposes
+// let board = [
 //   [0, 0, 0, 0, 0],
 //   [1, 0, 0, 2, 2],
 //   [1, 1, 1, 0, 0],
 //   [1, 1, 1, 0, 0],
 //   [1, 1, 1, 1, 0]
 // ];
-// // console.log(utilityFunction(state));
-// console.log(findAvailablePositions(2, 2, state).sort());
-
+// console.log(utilityFunction(board));
+// console.log(findAvailablePositions(2, 2, board).sort());
